9be2d2aa3b9d800a065e54d4a918b4c951049c3a
NiklasKahr <niklaskahr01@gmail.com>
2022-11-10 09:56:21 +0100

    card design

--- script.js
+++ script.js
@@ -20,4 +20,10 @@ function renderCard() {
     //document.getElementById('picture').src = currentPokemon['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
 
     document.getElementById('info-text').innerHTML = 'Text';
+
+    document.getElementById('type').innerHTML =
+        currentPokemon['types'][0]['type']['name'].charAt(0).toUpperCase() + currentPokemon['types'][0]['type']['name'].slice(1);
+    document.getElementById('weight').innerHTML = (currentPokemon['weight'] / 10).toFixed(1) + '<span class="font-14px">kg</span>';
+    document.getElementById('height').innerHTML = (currentPokemon['height'] / 10).toFixed(2) + '<span class="font-14px">m</span>';
+
 }
