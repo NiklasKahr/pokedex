ea45165562d378ead517c110ea00d177dae31728
NiklasKahr <niklaskahr01@gmail.com>
2022-11-23 10:51:38 +0100

    code optimization

--- script.js
+++ script.js
@@ -4,13 +4,6 @@ let isAmerican;
 
 
 async function init() {
-    /*let url = 'https://pokeapi.co/api/v2/pokemon/1/';
-    let response = await fetch(url);
-    currentPokemon = await response.json();
-
-    console.log('Loaded Pokémon:', currentPokemon)*/
-
-    //renderCard();
     await fillArray();
     renderContent();
 }
@@ -18,24 +11,13 @@ async function init() {
 
 async function fillArray() {
     for (let i = 0; i < 30; i++) { // max of 905
-        url = `https://pokeapi.co/api/v2/pokemon/${i + 1}/`;
+        url = `https://pokeapi.co/api/v2/pokemon/${i + 1}/`; //152
         let response = await fetch(url);
         currentPokemon = await response.json();
 
         pokemons.push(currentPokemon);
     }
 }
-
-
-/*async function fillArray() {
-    for (let i = 0; i < 30; i++) { // max of 905
-        url = `https://pokeapi.co/api/v2/pokemon/${i + 152}/`;
-        let response = await fetch(url);
-        currentPokemon = await response.json();
-
-        pokemons.push(currentPokemon);
-    }
-}*/
 
 
 // render functions
@@ -45,7 +27,7 @@ function renderContent() {
     for (let i = 0; i < pokemons.length; i++) {
         let pokemon = pokemons[i];
         document.getElementById('content').innerHTML += `
-        <div onclick="renderCard()" id="card${i}" class="content-card px-2 py-3 m-2 shadow-sm">
+        <div onclick="renderCard(${i})" id="card${i}" class="content-card px-2 py-3 m-2 shadow-sm">
             <div class="text-align-center">
                 <h5 id="content-name${i}" class="mb-n0_2">${pokemon['name'].charAt(0).toUpperCase() + pokemon['name'].slice(1)}</h5>
                 <span id="content-id${i}">#${pokemon['id']}</span>
@@ -63,7 +45,7 @@ function renderContent() {
 function addTypeColor(pokemon, cardId) {
     let card = document.getElementById(cardId);
 
-    if (type0EqualsNormal(pokemon) &&  hasType1(pokemon)) {
+    if (type0EqualsNormal(pokemon) && hasType1(pokemon)) {
         let type1 = pokemon['types'][1]['type']['name'];
         addBackgroundColor(card, type1);
     } else {
@@ -131,73 +113,85 @@ function addBackgroundColor(card, type) {
             card.classList.add('background-dark');
             break;
         default:
-            console.log('Unknown type: ', type0)
+            console.log('Unknown type: ', type)
             break;
     }
 }
 
 
-function renderCard() {
-    renderCredentials();
-    renderAttacks();
-    renderProperties();
+function renderCard(i) {
+    let pokemon = pokemons[i];
+    renderCredentials(pokemon);
+    renderAttacks(pokemon);
+    renderProperties(pokemon);
+    if (!buttonExists()) {
+        document.getElementById('selected-card').innerHTML += `
+        <button onclick="convertUnits(${i})" id="unit-button" class="btn btn-white shadow-sm">lb/ft</button>
+        `;
+    }
+    convertToInternational(pokemon)
     document.getElementById('selected-card').classList.remove('d-none');
 }
 
 
-function renderCredentials() {
-    document.getElementById('name').innerHTML = currentPokemon['name'].charAt(0).toUpperCase() + currentPokemon['name'].slice(1);
-    document.getElementById('id').innerHTML = '#' + currentPokemon['id'];
-    document.getElementById('sprite').src = currentPokemon['sprites']['other']['official-artwork']['front_default'];
+function renderCredentials(pokemon) {
+    document.getElementById('name').innerHTML = pokemon['name'].charAt(0).toUpperCase() + pokemon['name'].slice(1);
+    document.getElementById('id').innerHTML = '#' + pokemon['id'];
+    document.getElementById('sprite').src = pokemon['sprites']['other']['official-artwork']['front_default'];
 }
 
 
-function renderAttacks() {
-    let attack0Name = currentPokemon['abilities'][0]['ability']['name'];
-    let attack1Name = currentPokemon['abilities'][1]['ability']['name'];
-
+function renderAttacks(pokemon) {
+    let attack0Name = pokemon['abilities'][0]['ability']['name'];
     document.getElementById('attacks-text0').innerHTML = attack0Name.charAt(0).toUpperCase() + attack0Name.slice(1);
-    document.getElementById('attacks-text1').innerHTML = attack1Name.charAt(0).toUpperCase() + attack1Name.slice(1);
 
-    renderAttacks3And4();
+    renderAttacks1To3(pokemon);
 }
 
 
-function renderAttacks3And4() {
-    if (attack2Undefined()) { // at ln ...
+function renderAttacks1To3(pokemon) {
+
+    if (attackUndefined(pokemon, 1)) { // at ln ...
+        document.getElementById('attacks-text1').classList.add('d-none');
+    } else {
+        let attack1Name = pokemon['abilities'][1]['ability']['name'];
+        //document.getElementById('attacks-text1').classList.remove('d-none');
+        document.getElementById('attacks-text1').innerHTML = attack1Name.charAt(0).toUpperCase() + attack1Name.slice(1);
+    }
+
+    if (attackUndefined(pokemon, 2)) { // at ln ...
         document.getElementById('attacks-text2').classList.add('d-none');
     } else {
-        let attack2Name = currentPokemon['abilities'][2]['ability']['name'];
+        let attack2Name = pokemon['abilities'][2]['ability']['name'];
         //document.getElementById('attacks-text2').classList.remove('d-none');
         document.getElementById('attacks-text2').innerHTML = attack2Name.charAt(0).toUpperCase() + attack2Name.slice(1);
     }
 
-    if (attack3Undefined()) { // at ln ...
+    if (attackUndefined(pokemon, 3)) { // at ln ...
         document.getElementById('attacks-text3').classList.add('d-none');
     } else {
-        let attack3Name = currentPokemon['abilities'][3]['ability']['name'];
+        let attack3Name = pokemon['abilities'][3]['ability']['name'];
         //document.getElementById('attacks-text3').classList.remove('d-none');
         document.getElementById('attacks-text3').innerHTML = attack3Name.charAt(0).toUpperCase() + attack3Name.slice(1);
     }
 }
 
 
-function renderProperties() {
+function renderProperties(pokemon) {
     let type = document.getElementById('type');
     let weight = document.getElementById('weight');
     let height = document.getElementById('height');
 
     type.innerHTML =
-        currentPokemon['types'][0]['type']['name'].charAt(0).toUpperCase() + currentPokemon['types'][0]['type']['name'].slice(1);
+        pokemon['types'][0]['type']['name'].charAt(0).toUpperCase() + pokemon['types'][0]['type']['name'].slice(1);
     weight.innerHTML =
-        (currentPokemon['weight'] / 10).toFixed(1).replace('.', ',') + '<span class="font-14px ms-0_05">kg</span>';
+        (pokemon['weight'] / 10).toFixed(1).replace('.', ',') + '<span class="font-14px ms-0_05">kg</span>';
     height.innerHTML =
-        (currentPokemon['height'] / 10).toFixed(2).replace('.', ',') + '<span class="font-14px ms-0_05">m</span>';
+        (pokemon['height'] / 10).toFixed(2).replace('.', ',') + '<span class="font-14px ms-0_05">m</span>';
 }
 
 
 // conditional functions
-
 function type0EqualsNormal(pokemon) {
     return pokemon['types'][0]['type']['name'] == 'normal';
 }
@@ -206,30 +200,29 @@ function hasType1(pokemon) {
     return pokemon['types'][1];
 }
 
-
-function attack2Undefined() {
-    return currentPokemon['abilities'][2] == undefined;
+function attackUndefined(pokemon, attackNumber) {
+    return pokemon['abilities'][attackNumber] == undefined;
 }
 
-
-function attack3Undefined() {
-    return currentPokemon['abilities'][3] == undefined;
+function buttonExists() {
+    return document.getElementById('unit-button');
 }
 
 
 // convert functions
-function convertUnits() {
+function convertUnits(i) {
+    let pokemon = pokemons[i]
     if (isAmerican) {
-        convertToInternational();
+        convertToInternational(pokemon);
     } else {
         convertToAmerican();
     }
 }
 
 
-function convertToInternational() {
-    document.getElementById('weight').innerHTML = (currentPokemon['weight'] / 10).toFixed(1).replace('.', ',') + '<span class="font-14px ms-0_05">kg</span>';
-    document.getElementById('height').innerHTML = (currentPokemon['height'] / 10).toFixed(2).replace('.', ',') + '<span class="font-14px ms-0_05">m</span>';
+function convertToInternational(pokemon) {
+    document.getElementById('weight').innerHTML = (pokemon['weight'] / 10).toFixed(1).replace('.', ',') + '<span class="font-14px ms-0_05">kg</span>';
+    document.getElementById('height').innerHTML = (pokemon['height'] / 10).toFixed(2).replace('.', ',') + '<span class="font-14px ms-0_05">m</span>';
 
     document.getElementById('unit-button').innerHTML = 'lb/ft';
     isAmerican = false;
